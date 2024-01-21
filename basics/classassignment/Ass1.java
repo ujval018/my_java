e82e97b97ac699609eb532ebb9ca67b188c97dbc
ujval018 <77689691+ujval018@users.noreply.github.com>
2024-01-21 06:34:15 +0000

    ass1

--- basics/classassignment/Ass1.java
+++ basics/classassignment/Ass1.java
@@ -1,11 +1,11 @@
 //WAP in java which will accept KIIT UNIVERSITY as the command argument and display the same.
 
-import java.lang.*;
+
 
 public class Ass1{
 
     public static void main(String arg[]){
-        for(int i=0;i<=arg.strlength();i++)
+        for(int i=0;i<arg.length;i++)
         {
             System.out.println(arg[i]);
         }
